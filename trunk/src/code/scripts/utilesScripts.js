fc6509b5eb7ef4936e1da7f10c17a3e1234d56af
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2013-08-03 19:18:17 +0000

    PRUEBAS

--- trunk/src/code/scripts/utilesScripts.js
+++ trunk/src/code/scripts/utilesScripts.js
@@ -1,3 +1,4 @@
 alert("BIENVENIDO AL BLOG DE LA LOCURA DE MAO");
-var aux = document.getElementById(Header1_headerimg);
-aux.width = "900";
+//var aux = document.getElementById(Header1_headerimg);
+//aux.width = "900";
+document.getElementById(Header1_headerimg)..width = "900";
