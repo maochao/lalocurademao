34f56aace577075d0038182525bc966bca59776e
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2013-08-05 19:50:18 +0000

    Se suben todos los anuncios de EBAY

--- trunk/src/code/scripts/utilesScripts.js
+++
@@ -1,4 +0,0 @@
-alert("BIENVENIDO AL BLOG DE LA LOCURA DE MAO");
-//var aux = document.getElementById(Header1_headerimg);
-//aux.width = "900";
-document.getElementById(Header1_headerimg).width = "900";
