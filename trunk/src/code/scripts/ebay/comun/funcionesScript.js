2bc04d0eded9d8ed985a13d36c20ffac584bf9a1
maochao89 <maochao89@340ac609-711b-835c-51c5-9b311aa96c5f>
2013-08-25 20:29:20 +0000

    PRUEBAS PARA LOS ANUNCIOS DE EBAY

---
+++ trunk/src/code/scripts/ebay/comun/funcionesScript.js
@@ -0,0 +1,3 @@
+var tipoBlogExterna = "FITNESS";
+alert(tipoBlogExterna);
+document.getElementById('enlaceAnuncio').href = 'http://lalocurademao.blogspot.com.es/2012/05/cromos-album-liga-espanola-8687-este.html';
