2247c3f0180175e337f9341fcafd48e6541444b2
maochao89 <maochao89@340ac609-711b-835c-51c5-9b311aa96c5f>
2013-08-25 20:29:53 +0000

    PRUEBAS PARA LOS ANUNCIOS DE EBAY

--- src/code/scripts/blog/utilesScripts.js
+++ src/code/scripts/blog/utilesScripts.js
@@ -1,12 +1,14 @@
-//var aux = document.getElementById(Header1_headerimg);
-//aux.width = "900";
-//var tipoBlog = "FITNESS";
 var tipoBlog = "PUNK";
+// var tipoBlog = "FITNESS";
+
+if (typeof tipoBlogExterna != "undefined") {
+	tipoBlog = tipoBlogExterna;
+}
 
 if (tipoBlog == "PUNK") {
 	// ASPECTOS BLOG PUNK
 	document.body.style.background = "url('http://sites.google.com/site/lalocurademao/scripts/fondo5.jpg') repeat scroll left top #000000";
-} else if (tipoBlog == "FITNESS"){
+} else if (tipoBlog == "FITNESS") {
 	// ASPECTOS BLOG FITNESS
 	document.body.style.background = "url('http://sites.google.com/site/lalocurademao/scripts/fondoFit.jpg') repeat scroll left top #000000";
 	document.getElementById('Header1_headerimg').src = "http://sites.google.com/site/lalocurademao/scripts/logo1BlogspotLARGO.png";
