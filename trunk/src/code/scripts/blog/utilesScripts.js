aa90249a2bc75ab7495596b3bdcf77f4e49adfd7
maochao89 <maochao89@340ac609-711b-835c-51c5-9b311aa96c5f>
2013-08-25 20:13:35 +0000

    PRUEBAS CON CSS PARA EL BLOG

--- trunk/src/code/scripts/blog/utilesScripts.js
+++ trunk/src/code/scripts/blog/utilesScripts.js
@@ -1,4 +1,13 @@
 //var aux = document.getElementById(Header1_headerimg);
 //aux.width = "900";
-document.getElementById('Header1_headerimg').width = "600";
-document.body.style.color = "brown";
+//var tipoBlog = "FITNESS";
+var tipoBlog = "PUNK";
+
+if (tipoBlog == "PUNK") {
+	// ASPECTOS BLOG PUNK
+	document.body.style.background = "url('http://sites.google.com/site/lalocurademao/scripts/fondo5.jpg') repeat scroll left top #000000";
+} else if (tipoBlog == "FITNESS"){
+	// ASPECTOS BLOG FITNESS
+	document.body.style.background = "url('LA%20LOCURA%20DE%20MAO_files/fondoFit.jpg') repeat scroll left top #000000";
+	document.getElementById('Header1_headerimg').src = "LA%20LOCURA%20DE%20MAO_files/logo1BlogspotLARGO.png";
+}
