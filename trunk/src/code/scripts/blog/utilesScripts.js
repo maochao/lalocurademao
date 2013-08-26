75a2f59723f344604b30d59b25c503236629f20b
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2013-08-26 20:16:02 +0000

    PRUEBAS: cambiando propiedades en blogger dinámicamente.

--- trunk/src/code/scripts/blog/utilesScripts.js
+++ trunk/src/code/scripts/blog/utilesScripts.js
@@ -1,7 +1,8 @@
 var tipoBlog = "PUNK";
 //var tipoBlog = "FITNESS";
 
-//var ruta = "LA%20LOCURA%20DE%20MAO_files/";
+//var ruta = "LA%20LOCURA%20DE%20MAO_VAIO_files/";
+// var ruta = "LA%20LOCURA%20DE%20MAO_files/";
 var ruta = "http://sites.google.com/site/lalocurademao/scripts/";
 
 if (typeof tipoBlogExterna != "undefined") {
@@ -10,10 +11,16 @@ if (typeof tipoBlogExterna != "undefined") {
 
 if (tipoBlog == "PUNK") {
 	// ASPECTOS BLOG PUNK
-	document.body.style.background = "url(' "+ ruta +"fondo5.jpg') repeat scroll left top #000000";
-	document.getElementById('Header1_headerimg').src = ruta + "GrayscaleEdgyPunkRock_tituloBlog.png";
+	document.body.style.background = "url(' " + ruta
+			+ "fondo5.jpg') repeat scroll left top #000000";
+	document.body.style.color = "pink";
+	document.getElementById('Header1_headerimg').src = ruta
+			+ "GrayscaleEdgyPunkRock_tituloBlog.png";
 } else if (tipoBlog == "FITNESS") {
 	// ASPECTOS BLOG FITNESS
-	document.body.style.background = "url(' "+ ruta +"fondoFit.jpg') repeat scroll left top #000000";
-	document.getElementById('Header1_headerimg').src = ruta + "logo1BlogspotLARGO.png";
+	document.body.style.background = "url(' " + ruta
+			+ "fondoFit.jpg') repeat scroll left top #000000";
+	document.getElementById('Header1_headerimg').src = ruta
+			+ "logo1BlogspotLARGO.png";
+	document.body.style.color = "purple";
 }
