3a16fca0c20a99695e4550b18513c9e1ee3d29eb
maochao89 <maochao89@340ac609-711b-835c-51c5-9b311aa96c5f>
2013-08-26 15:54:54 +0000

    ESTABLECIDA DIFERENCIA PARA TEMÁTICA PUNK Y TEMÁTICA FITNESS

--- trunk/src/code/scripts/blog/utilesScripts.js
+++ trunk/src/code/scripts/blog/utilesScripts.js
@@ -1,5 +1,8 @@
-var tipoBlog = "PUNK";
-// var tipoBlog = "FITNESS";
+//var tipoBlog = "PUNK";
+var tipoBlog = "FITNESS";
+
+var ruta = "LA%20LOCURA%20DE%20MAO_files/";
+//var ruta = "http://sites.google.com/site/lalocurademao/scripts/";
 
 if (typeof tipoBlogExterna != "undefined") {
 	tipoBlog = tipoBlogExterna;
@@ -7,9 +10,10 @@ if (typeof tipoBlogExterna != "undefined") {
 
 if (tipoBlog == "PUNK") {
 	// ASPECTOS BLOG PUNK
-	document.body.style.background = "url('http://sites.google.com/site/lalocurademao/scripts/fondo5.jpg') repeat scroll left top #000000";
+	document.body.style.background = "url(' "+ ruta +"fondo5.jpg') repeat scroll left top #000000";
+	document.getElementById('Header1_headerimg').src = ruta + "GrayscaleEdgyPunkRock_tituloBlog.png";
 } else if (tipoBlog == "FITNESS") {
 	// ASPECTOS BLOG FITNESS
-	document.body.style.background = "url('http://sites.google.com/site/lalocurademao/scripts/fondoFit.jpg') repeat scroll left top #000000";
-	document.getElementById('Header1_headerimg').src = "http://sites.google.com/site/lalocurademao/scripts/logo1BlogspotLARGO.png";
+	document.body.style.background = "url(' "+ ruta +"fondoFit.jpg') repeat scroll left top #000000";
+	document.getElementById('Header1_headerimg').src = ruta + "logo1BlogspotLARGO.png";
 }
