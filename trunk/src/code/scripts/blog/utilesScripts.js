5783f899f05c7861f8089954ca77cd42cbad1acc
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2013-09-24 20:54:11 +0000

    CSS DE DOS TIPOS: PUNK/FITNESS

--- trunk/src/code/scripts/blog/utilesScripts.js
+++ trunk/src/code/scripts/blog/utilesScripts.js
@@ -5,45 +5,12 @@ var tipoBlog = "FITNESS";
 //var ruta = "LA%20LOCURA%20DE%20MAO_VAIO_files/";
 // var ruta = "LA%20LOCURA%20DE%20MAO_files/";
 var ruta = "http://sites.google.com/site/lalocurademao/scripts/";
-var rutaCss = "https://lalocurademao.googlecode.com/svn/trunk/src/code/scripts/blog/css/utilesCss.css";
+var rutaCssPunk = "https://lalocurademao.googlecode.com/svn/trunk/src/code/scripts/blog/css/utilesCssPunk.css";
+var rutaCssFitness = "https://lalocurademao.googlecode.com/svn/trunk/src/code/scripts/blog/css/utilesCssFitness.css";
 
-if (typeof tipoBlogExterna != "undefined") {
-	tipoBlog = tipoBlogExterna;
-}
-
-if (tipoBlog == "PUNK") {
-	// ASPECTOS BLOG PUNK
-	document.body.style.background = "url(' " + ruta
-			+ "fondo5.jpg') repeat scroll left top #000000";
-	document.body.style.color = "pink";
-	document.getElementById('Header1_headerimg').src = ruta
-			+ "GrayscaleEdgyPunkRock_tituloBlog.png";
-} else if (tipoBlog == "FITNESS") {
-	// ASPECTOS BLOG FITNESS
-	document.body.style.background = "url(' " + ruta
-			+ "fondoFit.jpg') repeat scroll left top #000000";
-	document.getElementById('Header1_headerimg').src = ruta
-			+ "logo1BlogspotLARGO.png";
-	document.getElementById('Header1_headerimg').width = "790";
-	//document.body.style.color = "black";
-	//document.getElementById('fondoEntrada').style.backgroundColor = "lavender";
-}
-
-cargarFicheroCss(rutaCss, "css");
-//aniadirHojaEstilos();
-
-function aniadirHojaEstilos(){
-  if(document.createStyleSheet) {
-    document.createStyleSheet('https://lalocurademao.googlecode.com/svn/trunk/src/code/scripts/blog/css/utilesCss.css');
-  }
-  else {
-    var styles = "@import url('https://lalocurademao.googlecode.com/svn/trunk/src/code/scripts/blog/css/utilesCss.css');";
-    var newSS=document.createElement('link');
-    newSS.rel='stylesheet';
-    newSS.href='data:text/css,'+escape(styles);
-    document.getElementsByTagName("head")[0].appendChild(newSS);
-  }
-}
+//if (typeof tipoBlogExterna != "undefined") {
+//	tipoBlog = tipoBlogExterna;
+//}
 
 function cargarFicheroCss(filename, filetype) {
     if (filetype == "js") { //if filename is a external JavaScript file
@@ -61,4 +28,23 @@ function cargarFicheroCss(filename, filetype) {
     }
     if (typeof fileref != "undefined")
         document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+
+if (tipoBlog == "PUNK") {
+	// ASPECTOS BLOG PUNK
+	document.body.style.background = "url(' " + ruta
+			+ "fondo5.jpg') repeat scroll left top #000000";
+	document.body.style.color = "pink";
+	document.getElementById('Header1_headerimg').src = ruta
+			+ "GrayscaleEdgyPunkRock_tituloBlog.png";
+	cargarFicheroCss(rutaCssPunk, "css");
+	
+} else if (tipoBlog == "FITNESS") {
+	// ASPECTOS BLOG FITNESS
+	document.body.style.background = "url(' " + ruta
+			+ "fondoFit.jpg') repeat scroll left top #000000";
+	document.getElementById('Header1_headerimg').src = ruta
+			+ "logo1BlogspotLARGO.png";
+	document.getElementById('Header1_headerimg').width = "790";
+	cargarFicheroCss(rutaCssFitness, "css");
 }
