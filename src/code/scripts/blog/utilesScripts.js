800042af9c4907e8c48aa071bfc031692eb2f4b5
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2014-01-29 16:32:13 +0000

    PRUEBAS PARA EVITAR EL ERROR DE CROSSDOMAIN CON CORS

--- src/code/scripts/blog/utilesScripts.js
+++ src/code/scripts/blog/utilesScripts.js
@@ -66,6 +66,51 @@ xhttp.send();
 return xhttp.responseText;
 }
 
+//Función para encapsular la detección del objeto que tenemos que usar para AJAX inter-dominios, según el navegador en el que estemos.
+function createCorsObject(){
+
+//Inicialmente creamos XHR
+var xhrObject = new XMLHttpRequest();
+    //comprobamos si XHR tiene capacidades CORS o es el antiguo
+    if ("withCredentials" in xhrObject){
+        return xhrObject;
+    }
+    //si es el antiguo, comprobamos si el navegador soporta el objeto XDR
+    else if (typeof XDomainRequest != "undefined"){
+         xhrObject = new XDomainRequest();
+    } else {
+    xhrObject = null;
+    }
+return xhrObject;
+}
+
+
+function loadXMLDocCors(filename)
+{
+	//obtenemos un objeto para AJAX cross-dominio
+	var xhrObject = createCorsObject();
+	
+	//si tenemos un objeto válido...
+	if (xhrObject){
+	
+	//definimos los parámetros de la petición HTTP
+	xhrObject.open("get", filename);
+	
+	//definimos un callback para tratar los datos que recibamos
+	xhrObject.onload = function(){
+		alert(xhrObjet.responseText)
+	};
+	
+	//definimos un callback para tratar el caso de error
+	xhrObject.onerror = function(){
+	    // código para caso de error
+	};
+	
+	//Enviamos la petición
+	xhrObject.send(null);
+	}
+}
+
 var filename = 'https://lalocurademao.googlecode.com/svn/trunk/src/code/scripts/blog/datos/archivoDatos';
-var xmlDoc = loadXMLDoc(filename);
+var xmlDoc = loadXMLDocCors(filename);
 alert(xmlDoc);
