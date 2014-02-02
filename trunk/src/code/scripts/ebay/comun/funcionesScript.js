14535e4aa8b44aac6278dd7973f80f39160589ba
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2014-02-02 18:46:31 +0000

    Pruebas de scripts para TodoColecciones

--- trunk/src/code/scripts/ebay/comun/funcionesScript.js
+++ trunk/src/code/scripts/ebay/comun/funcionesScript.js
@@ -1,3 +1,1 @@
-var tipoBlogExterna = "FITNESS";
-alert(tipoBlogExterna);
-document.getElementById('enlaceAnuncio').href = 'http://lalocurademao.blogspot.com.es/2012/05/cromos-album-liga-espanola-8687-este.html';
+alert("mao");
