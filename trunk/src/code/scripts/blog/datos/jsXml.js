8a812566659db24072892ea01f9b39a475c4ea52
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2014-02-03 16:48:59 +0000

    Fichero para pruebas en local con el script para la creacion de los nodos de la entrada de RANKING GIMNASIOS

---
+++ trunk/src/code/scripts/blog/datos/jsXml.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {cargarDatos()});
+
+function cargarDatos () {
+
+_xml =  $.parseXML(archivoGimnasios);
+$xmlDoc = $(_xml);
+$('#tabla tbody tr').remove();
+$xmlDoc.find("gimnasio").each(function (){
+		
+		var x = '<tr>';
+		
+		var nombre = $(this).find("nombre").text();
+		var textoNombre = '<td><span>'+nombre+'</span></td>';
+		texto = texto + textoNombre;
+		
+		var nota = $(this).find("nota").text();
+		var textoNota = '<td><span>'+nota+'</span></td>';
+		texto = texto + textoNota;
+		
+		var taquillas = $(this).find("taquillas").text();
+		var textoTaquilllas = '<td><span>'+taquillas+'</span></td>';
+		texto = texto + textoTaquilllas;
+		
+		texto = texto + '</tr>';
+		
+		$('#tabla tbody').append(texto);
+})
+};
