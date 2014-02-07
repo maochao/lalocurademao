d2f980f54fcee3444f3224e65018622c43c8716e
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2014-02-07 14:10:42 +0000

    070214 - INTRODUCIENDO URL EN EL NOMBRE DEL GIMNASIO

--- trunk/src/code/scripts/blog/datos/jsGimnasios.js
+++ trunk/src/code/scripts/blog/datos/jsGimnasios.js
@@ -1,1 +1,20 @@
 var archivoGimnasios = "<gimnasios><gimnasio><nombre>MCFIT</nombre><url>http://lalocurademao.blogspot.com.es/2013/06/review-mcfit-leganes.html</url><nota>10</nota><taquillas>200</taquillas></gimnasio><gimnasio><nombre>ALTAFIT</nombre><nota>8</nota><taquillas>300</taquillas></gimnasio><gimnasio><nombre>BASICFIT</nombre><nota>9</nota><taquillas>250</taquillas></gimnasio></gimnasios>";
+
+//http://lalocurademao.blogspot.com.es/2014/02/review-mcfit-ventas.html
+//http://lalocurademao.blogspot.com.es/2014/01/review-smart-fit-coslada.html
+//http://lalocurademao.blogspot.com.es/2014/01/review-fitness-place-alameda-osuna.html
+//http://lalocurademao.blogspot.com.es/2014/01/review-mcfit-el-carmen.html
+//http://lalocurademao.blogspot.com.es/2014/01/review-ojasport-haro.html
+//http://lalocurademao.blogspot.com.es/2014/01/review-sentral-miranda-de-ebro.html
+//http://lalocurademao.blogspot.com.es/2013/12/review-health-city-basic-fit-orense.html
+//http://lalocurademao.blogspot.com.es/2013/11/review-star-sport-wellnes-club.html
+//http://lalocurademao.blogspot.com.es/2013/11/review-body-care-hesperia-lanzarote.html
+//http://lalocurademao.blogspot.com.es/2013/10/review-corpowellness-telefonica.html
+//http://lalocurademao.blogspot.com.es/2013/09/review-fitness-center-vaguada.html
+//http://lalocurademao.blogspot.com.es/2013/08/review-castellana-sport-club.html
+//http://lalocurademao.blogspot.com.es/2013/06/review-mcfit-leganes.html
+//http://lalocurademao.blogspot.com.es/2013/06/review-alta-fit-las-mercedes.html
+//http://lalocurademao.blogspot.com.es/2013/05/review-holidays-gym.html
+//http://lalocurademao.blogspot.com.es/2013/04/review-o2-plenilunio.html
+//http://lalocurademao.blogspot.com.es/2013/04/review-palestra-gym-rivas.html
+//http://lalocurademao.blogspot.com.es/2011/10/cuidado-con-los-gimnasios.html
