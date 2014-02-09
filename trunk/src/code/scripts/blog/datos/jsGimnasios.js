2246bc690d46f20e1f2896f63f1fd9c7b16fd931
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2014-02-09 21:31:36 +0000

    SE AÑADEN NUEVOS GIMNASIOS AL JS DE GIMNASIOS

--- trunk/src/code/scripts/blog/datos/jsGimnasios.js
+++ trunk/src/code/scripts/blog/datos/jsGimnasios.js
@@ -1,20 +1,202 @@
-var archivoGimnasios = "<gimnasios><gimnasio><nombre>MCFIT</nombre><url>http://lalocurademao.blogspot.com.es/2013/06/review-mcfit-leganes.html</url><nota>10</nota><taquillas>200</taquillas></gimnasio><gimnasio><nombre>ALTAFIT</nombre><nota>8</nota><taquillas>300</taquillas></gimnasio><gimnasio><nombre>BASICFIT</nombre><nota>9</nota><taquillas>250</taquillas></gimnasio></gimnasios>";
+var archivoGimnasios = "" +
+		"<gimnasios>" +
 
-//http://lalocurademao.blogspot.com.es/2014/02/review-mcfit-ventas.html
-//http://lalocurademao.blogspot.com.es/2014/01/review-smart-fit-coslada.html
-//http://lalocurademao.blogspot.com.es/2014/01/review-fitness-place-alameda-osuna.html
-//http://lalocurademao.blogspot.com.es/2014/01/review-mcfit-el-carmen.html
-//http://lalocurademao.blogspot.com.es/2014/01/review-ojasport-haro.html
-//http://lalocurademao.blogspot.com.es/2014/01/review-sentral-miranda-de-ebro.html
-//http://lalocurademao.blogspot.com.es/2013/12/review-health-city-basic-fit-orense.html
-//http://lalocurademao.blogspot.com.es/2013/11/review-star-sport-wellnes-club.html
-//http://lalocurademao.blogspot.com.es/2013/11/review-body-care-hesperia-lanzarote.html
-//http://lalocurademao.blogspot.com.es/2013/10/review-corpowellness-telefonica.html
-//http://lalocurademao.blogspot.com.es/2013/09/review-fitness-center-vaguada.html
-//http://lalocurademao.blogspot.com.es/2013/08/review-castellana-sport-club.html
-//http://lalocurademao.blogspot.com.es/2013/06/review-mcfit-leganes.html
-//http://lalocurademao.blogspot.com.es/2013/06/review-alta-fit-las-mercedes.html
-//http://lalocurademao.blogspot.com.es/2013/05/review-holidays-gym.html
-//http://lalocurademao.blogspot.com.es/2013/04/review-o2-plenilunio.html
-//http://lalocurademao.blogspot.com.es/2013/04/review-palestra-gym-rivas.html
-//http://lalocurademao.blogspot.com.es/2011/10/cuidado-con-los-gimnasios.html
+			"<gimnasio>" +
+				"<nombre>MCFIT LEGANES</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/06/review-mcfit-leganes.html</url>" +
+				"<nota>10</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>200</taquillas>" +
+			"</gimnasio>" +
+		
+			"<gimnasio>" +
+				"<nombre>ALTAFIT POL. LAS MERCEDES</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/06/review-alta-fit-las-mercedes.html</url>" +
+				"<nota>8</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>300</taquillas>" +
+			"</gimnasio>" +
+			
+			"<gimnasio>" +
+				"<nombre>BASICFIT ORENSE</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/12/review-health-city-basic-fit-orense.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +
+			
+			"<gimnasio>" +
+				"<nombre>SENTRAL MIRANDA DE EBRO</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2014/01/review-sentral-miranda-de-ebro.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +
+			
+			"<gimnasio>" +
+				"<nombre>OJASPORT HARO</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2014/01/review-ojasport-haro.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +			
+			
+			"<gimnasio>" +
+				"<nombre>MCFIT VENTAS</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2014/02/review-mcfit-ventas.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +
+		
+			"<gimnasio>" +
+				"<nombre>MCFIT EL CARMEN</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2014/01/review-mcfit-el-carmen.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +
+	
+			"<gimnasio>" +
+				"<nombre>FITNESS PLACE ALAMEDA OSUNA</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2014/01/review-fitness-place-alameda-osuna.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +
+
+			"<gimnasio>" +
+				"<nombre>SMART FIT COSLADA</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2014/01/review-smart-fit-coslada.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +
+			
+			"<gimnasio>" +
+				"<nombre>STAR SPORT </nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/11/review-star-sport-wellnes-club.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +			
+			
+			"<gimnasio>" +
+				"<nombre>BODY CARE HESPERIA LANZAROTE</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/11/review-body-care-hesperia-lanzarote.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +			
+			
+			"<gimnasio>" +
+				"<nombre>CORPOWELLNESS TELEFONICA</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/10/review-corpowellness-telefonica.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +
+			
+			"<gimnasio>" +
+				"<nombre>FITNESS CENTER VAGUADA</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/09/review-fitness-center-vaguada.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +			
+			
+			"<gimnasio>" +
+				"<nombre>CASTELLANA SPORT CLUB</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/08/review-castellana-sport-club.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +			
+			
+			"<gimnasio>" +
+				"<nombre>HOLIDAYS GYM MADRID</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/05/review-holidays-gym.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +			
+			
+			"<gimnasio>" +
+				"<nombre>O2 PLENILUNIO</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/04/review-o2-plenilunio.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +
+		
+			"<gimnasio>" +
+				"<nombre>PALESTRA RIVAS</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/04/review-palestra-gym-rivas.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +
+	
+			"<gimnasio>" +
+				"<nombre>POLIDEPORTIVOS AND FITNESS</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2011/10/cuidado-con-los-gimnasios.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>10</mobiliario>" +
+				"<personal>10</personal>" +
+				"<precio>10</precio>" +
+				"<masificacion>10</masificacion>" +
+				"<taquillas>250</taquillas>" +
+			"</gimnasio>" +
+			
+		"</gimnasios>";
--- trunk/src/code/scripts/blog/datos/jsXml.js
+++ trunk/src/code/scripts/blog/datos/jsXml.js
@@ -15,12 +15,28 @@ $xmlDoc.find("gimnasio").each(function (){
 		texto = texto + textoNombre;
 		
 		var nota = $(this).find("nota").text();
-		var textoNota = '<td><span>'+nota+'</span></td>';
+		var textoNota = '<td align="center"><span>'+nota+'</span></td>';
 		texto = texto + textoNota;
 		
+		var mobiliario = $(this).find("mobiliario").text();
+		var textoMobiliario = '<td align="center"><span>'+mobiliario+'</span></td>';
+		texto = texto + textoMobiliario;
+		
+		var personal = $(this).find("personal").text();
+		var textoPersonal = '<td align="center"><span>'+personal+'</span></td>';
+		texto = texto + textoPersonal;
+		
+		var precio = $(this).find("precio").text();
+		var textoPrecio = '<td align="center"><span>'+precio+'</span></td>';
+		texto = texto + textoPrecio;
+		
+		var masificacion = $(this).find("masificacion").text();
+		var textoMasificacion = '<td align="center"><span>'+masificacion+'</span></td>';
+		texto = texto + textoMasificacion;
+		
 		var taquillas = $(this).find("taquillas").text();
-		var textoTaquilllas = '<td><span>'+taquillas+'</span></td>';
-		texto = texto + textoTaquilllas;
+		var textoTaquillas = '<td align="center"><span>'+taquillas+'</span></td>';
+		texto = texto + textoTaquillas;
 		
 		texto = texto + '</tr>';
 		
