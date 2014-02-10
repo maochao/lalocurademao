66df47d2515c53e0b936c23363f3062471d55254
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2014-02-10 11:55:43 +0000

    Nuevos datos para la lista de rankings.

--- trunk/src/code/scripts/blog/datos/jsGimnasios.js
+++ trunk/src/code/scripts/blog/datos/jsGimnasios.js
@@ -2,61 +2,6 @@ var archivoGimnasios = "" +
 		"<gimnasios>" +
 
 			"<gimnasio>" +
-				"<nombre>MCFIT LEGANES</nombre>" +
-				"<url>http://lalocurademao.blogspot.com.es/2013/06/review-mcfit-leganes.html</url>" +
-				"<nota>9</nota>" +
-				"<mobiliario>-</mobiliario>" +
-				"<personal>-</personal>" +
-				"<precio>-</precio>" +
-				"<masificacion>-</masificacion>" +
-				"<taquillas>-</taquillas>" +
-			"</gimnasio>" +
-		
-			"<gimnasio>" +
-				"<nombre>ALTAFIT POL. LAS MERCEDES</nombre>" +
-				"<url>http://lalocurademao.blogspot.com.es/2013/06/review-alta-fit-las-mercedes.html</url>" +
-				"<nota>-</nota>" +
-				"<mobiliario>-</mobiliario>" +
-				"<personal>-</personal>" +
-				"<precio>-</precio>" +
-				"<masificacion>-</masificacion>" +
-				"<taquillas>-</taquillas>" +
-			"</gimnasio>" +
-			
-			"<gimnasio>" +
-				"<nombre>BASICFIT ORENSE</nombre>" +
-				"<url>http://lalocurademao.blogspot.com.es/2013/12/review-health-city-basic-fit-orense.html</url>" +
-				"<nota>-</nota>" +
-				"<mobiliario>-</mobiliario>" +
-				"<personal>-</personal>" +
-				"<precio>-</precio>" +
-				"<masificacion>-</masificacion>" +
-				"<taquillas>-</taquillas>" +
-			"</gimnasio>" +
-			
-			"<gimnasio>" +
-				"<nombre>SENTRAL MIRANDA DE EBRO</nombre>" +
-				"<url>http://lalocurademao.blogspot.com.es/2014/01/review-sentral-miranda-de-ebro.html</url>" +
-				"<nota>-</nota>" +
-				"<mobiliario>-</mobiliario>" +
-				"<personal>-</personal>" +
-				"<precio>-</precio>" +
-				"<masificacion>-</masificacion>" +
-				"<taquillas>-</taquillas>" +
-			"</gimnasio>" +
-			
-			"<gimnasio>" +
-				"<nombre>OJASPORT HARO</nombre>" +
-				"<url>http://lalocurademao.blogspot.com.es/2014/01/review-ojasport-haro.html</url>" +
-				"<nota>-</nota>" +
-				"<mobiliario>-</mobiliario>" +
-				"<personal>-</personal>" +
-				"<precio>-</precio>" +
-				"<masificacion>-</masificacion>" +
-				"<taquillas>-</taquillas>" +
-			"</gimnasio>" +			
-			
-			"<gimnasio>" +
 				"<nombre>MCFIT VENTAS</nombre>" +
 				"<url>http://lalocurademao.blogspot.com.es/2014/02/review-mcfit-ventas.html</url>" +
 				"<nota>-</nota>" +
@@ -64,7 +9,13 @@ var archivoGimnasios = "" +
 				"<personal>-</personal>" +
 				"<precio>-</precio>" +
 				"<masificacion>-</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
 				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
 			"</gimnasio>" +
 		
 			"<gimnasio>" +
@@ -75,7 +26,13 @@ var archivoGimnasios = "" +
 				"<personal>-</personal>" +
 				"<precio>-</precio>" +
 				"<masificacion>-</masificacion>" +
-				"<taquillas>-</taquillas>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>551</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
 			"</gimnasio>" +
 	
 			"<gimnasio>" +
@@ -86,7 +43,30 @@ var archivoGimnasios = "" +
 				"<personal>-</personal>" +
 				"<precio>-</precio>" +
 				"<masificacion>-</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
 				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
+			"</gimnasio>" +
+
+			"<gimnasio>" +
+				"<nombre>BASICFIT ORENSE</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/12/review-health-city-basic-fit-orense.html</url>" +
+				"<nota>-</nota>" +
+				"<mobiliario>-</mobiliario>" +
+				"<personal>-</personal>" +
+				"<precio>-</precio>" +
+				"<masificacion>-</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
 			"</gimnasio>" +
 
 			"<gimnasio>" +
@@ -97,8 +77,48 @@ var archivoGimnasios = "" +
 				"<personal>-</personal>" +
 				"<precio>-</precio>" +
 				"<masificacion>-</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
 				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
 			"</gimnasio>" +
+
+			"<gimnasio>" +
+				"<nombre>SENTRAL MIRANDA DE EBRO</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2014/01/review-sentral-miranda-de-ebro.html</url>" +
+				"<nota>-</nota>" +
+				"<mobiliario>-</mobiliario>" +
+				"<personal>-</personal>" +
+				"<precio>-</precio>" +
+				"<masificacion>-</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
+			"</gimnasio>" +
+			
+			"<gimnasio>" +
+				"<nombre>OJASPORT HARO</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2014/01/review-ojasport-haro.html</url>" +
+				"<nota>-</nota>" +
+				"<mobiliario>-</mobiliario>" +
+				"<personal>-</personal>" +
+				"<precio>-</precio>" +
+				"<masificacion>-</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
+			"</gimnasio>" +		 
 			
 			"<gimnasio>" +
 				"<nombre>STAR SPORT </nombre>" +
@@ -108,7 +128,13 @@ var archivoGimnasios = "" +
 				"<personal>-</personal>" +
 				"<precio>-</precio>" +
 				"<masificacion>-</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
 				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
 			"</gimnasio>" +			
 			
 			"<gimnasio>" +
@@ -119,84 +145,302 @@ var archivoGimnasios = "" +
 				"<personal>-</personal>" +
 				"<precio>-</precio>" +
 				"<masificacion>-</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
 				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
 			"</gimnasio>" +			
-			
+
 			"<gimnasio>" +
-				"<nombre>CORPOWELLNESS TELEFONICA</nombre>" +
-				"<url>http://lalocurademao.blogspot.com.es/2013/-/review-corpowellness-telefonica.html</url>" +
+				"<nombre>ALTAFIT POL. LAS MERCEDES</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/06/review-alta-fit-las-mercedes.html</url>" +
 				"<nota>-</nota>" +
 				"<mobiliario>-</mobiliario>" +
 				"<personal>-</personal>" +
 				"<precio>-</precio>" +
 				"<masificacion>-</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
 				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
+			"</gimnasio>" +
+
+	  	"<gimnasio>" +
+				"<nombre>MCFIT LEGANES</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/06/review-mcfit-leganes.html</url>" +
+				"<nota>9</nota>" +
+				"<mobiliario>-</mobiliario>" +
+				"<personal>-</personal>" +
+				"<precio>-</precio>" +
+				"<masificacion>-</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+  			"<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
+			"</gimnasio>" +
+			
+			"<gimnasio>" +
+				"<nombre>CORPOWELLNESS TELEFONICA</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/10/review-corpowellness-telefonica.html</url>" +
+				"<nota>-</nota>" +
+				"<mobiliario>-</mobiliario>" +
+				"<personal>-</personal>" +
+				"<precio>-</precio>" +
+				"<masificacion>-</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
 			"</gimnasio>" +
 			
 			"<gimnasio>" +
 				"<nombre>FITNESS CENTER VAGUADA</nombre>" +
-				"<url>http://lalocurademao.blogspot.com.es/2013/0-/review-fitness-center-vaguada.html</url>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/09/review-fitness-center-vaguada.html</url>" +
 				"<nota>-</nota>" +
 				"<mobiliario>-</mobiliario>" +
 				"<personal>-</personal>" +
 				"<precio>-</precio>" +
 				"<masificacion>-</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
 				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
 			"</gimnasio>" +			
 			
 			"<gimnasio>" +
 				"<nombre>CASTELLANA SPORT CLUB</nombre>" +
 				"<url>http://lalocurademao.blogspot.com.es/2013/08/review-castellana-sport-club.html</url>" +
-				"<nota>-</nota>" +
-				"<mobiliario>-</mobiliario>" +
-				"<personal>-</personal>" +
-				"<precio>-</precio>" +
-				"<masificacion>-</masificacion>" +
-				"<taquillas>-</taquillas>" +
+				"<nota>6.8</nota>" +
+				"<mobiliario>8.6</mobiliario>" +
+				"<personal>9.5</personal>" +
+				"<precio>2.5</precio>" +
+				"<masificacion>8</masificacion>" +
+		  	"<localizacion>7</localizacion>" +
+			  "<mantenimiento>7</mantenimiento>" +
+			  "<sedes>1</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>761</taquillas>" +
+				"<duchas>14</duchas>" +
+				"<pesoMaxMancuerna>48</pesoMaxMancuerna>" +
 			"</gimnasio>" +			
 			
 			"<gimnasio>" +
-				"<nombre>HOLIDAYS GYM MADRID</nombre>" +
+				"<nombre>HOLIDAYS GYM AZCA</nombre>" +
 				"<url>http://lalocurademao.blogspot.com.es/2013/05/review-holidays-gym.html</url>" +
-				"<nota>-</nota>" +
-				"<mobiliario>-</mobiliario>" +
-				"<personal>-</personal>" +
-				"<precio>-</precio>" +
-				"<masificacion>-</masificacion>" +
+				"<nota>7.2</nota>" +
+				"<mobiliario>6</mobiliario>" +
+				"<personal>6.8</personal>" +
+				"<precio>8.5</precio>" +
+				"<masificacion>2</masificacion>" +
+		  	"<localizacion>9</localizacion>" +
+			  "<mantenimiento>3.5</mantenimiento>" +
+			  "<sedes>13</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>366</taquillas>" +
+				"<duchas>11</duchas>" +
+				"<pesoMaxMancuerna>40</pesoMaxMancuerna>" +
+			"</gimnasio>" +			
+
+			"<gimnasio>" +
+				"<nombre>HOLIDAYS GYM GOYA</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/05/review-holidays-gym.html</url>" +
+				"<nota>7.8</nota>" +
+				"<mobiliario>8.2</mobiliario>" +
+				"<personal>7</personal>" +
+				"<precio>8.5</precio>" +
+				"<masificacion>5</masificacion>" +
+		  	"<localizacion>8</localizacion>" +
+			  "<mantenimiento>8</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>320</taquillas>" +
+				"<duchas>14</duchas>" +
+				"<pesoMaxMancuerna>37.5</pesoMaxMancuerna>" +
+			"</gimnasio>" +		 
+
+			"<gimnasio>" +
+				"<nombre>HOLIDAYS GYM PRINCESA</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/05/review-holidays-gym.html</url>" +
+				"<nota>7</nota>" +
+				"<mobiliario>7</mobiliario>" +
+				"<personal>7</personal>" +
+				"<precio>8.5</precio>" +
+				"<masificacion>3</masificacion>" +
+		  	"<localizacion>7</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>13</sedes>" +
+			 "<extras>-</extras>" +
 				"<taquillas>-</taquillas>" +
+				"<duchas></duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
+			"</gimnasio>" +		
+
+			"<gimnasio>" +
+				"<nombre>HOLIDAYS GYM MONCLOA</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/05/review-holidays-gym.html</url>" +
+				"<nota>6.5</nota>" +
+				"<mobiliario>6.5</mobiliario>" +
+				"<personal>7</personal>" +
+				"<precio>8.5</precio>" +
+				"<masificacion>3</masificacion>" +
+		  	"<localizacion>7</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>-</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>-</taquillas>" +
+				"<duchas>12</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
+			"</gimnasio>" +		
+
+			"<gimnasio>" +
+				"<nombre>HOLIDAYS GYM HOYOS</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/05/review-holidays-gym.html</url>" +
+				"<nota>6.9</nota>" +
+				"<mobiliario>7</mobiliario>" +
+				"<personal>7</personal>" +
+				"<precio>8.5</precio>" +
+				"<masificacion>3</masificacion>" +
+		  	"<localizacion>8</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>13</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>-</taquillas>" +
+				"<duchas>10</duchas>" +
+				"<pesoMaxMancuerna>40</pesoMaxMancuerna>" +
+			"</gimnasio>" +		 
+
+			"<gimnasio>" +
+				"<nombre>HOLIDAYS GYM VERGARA</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/05/review-holidays-gym.html</url>" +
+				"<nota>6.9</nota>" +
+				"<mobiliario>7</mobiliario>" +
+				"<personal>-</personal>" +
+				"<precio>8.5</precio>" +
+				"<masificacion>4</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>13</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>223</taquillas>" +
+				"<duchas>18</duchas>" +
+				"<pesoMaxMancuerna>40</pesoMaxMancuerna>" +
+			"</gimnasio>" +		 
+
+			"<gimnasio>" +
+				"<nombre>HOLIDAYS GYM ALUCHE</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/05/review-holidays-gym.html</url>" +
+				"<nota>6.9</nota>" +
+				"<mobiliario>7</mobiliario>" +
+				"<personal>-</personal>" +
+				"<precio>8.5</precio>" +
+				"<masificacion>4</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>13</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>428</taquillas>" +
+				"<duchas>9</duchas>" +
+				"<pesoMaxMancuerna>50</pesoMaxMancuerna>" +
+			"</gimnasio>" +		 
+
+			"<gimnasio>" +
+				"<nombre>HOLIDAYS GYM GARDEN</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/05/review-holidays-gym.html</url>" +
+				"<nota>6.9</nota>" +
+				"<mobiliario>7</mobiliario>" +
+				"<personal>-</personal>" +
+				"<precio>8.5</precio>" +
+				"<masificacion>4</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>13</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>226</taquillas>" +
+				"<duchas>10</duchas>" +
+				"<pesoMaxMancuerna>50</pesoMaxMancuerna>" +
+			"</gimnasio>" +		 
+
+			"<gimnasio>" +
+				"<nombre>HOLIDAYS GYM ALBUFERA</nombre>" +
+				"<url>http://lalocurademao.blogspot.com.es/2013/05/review-holidays-gym.html</url>" +
+				"<nota>6.9</nota>" +
+				"<mobiliario>7</mobiliario>" +
+				"<personal>-</personal>" +
+				"<precio>8.5</precio>" +
+				"<masificacion>4</masificacion>" +
+		  	"<localizacion>-</localizacion>" +
+			  "<mantenimiento>-</mantenimiento>" +
+			  "<sedes>13</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>190</taquillas>" +
+				"<duchas>8</duchas>" +
+				"<pesoMaxMancuerna>47.5</pesoMaxMancuerna>" +
 			"</gimnasio>" +			
 			
 			"<gimnasio>" +
 				"<nombre>O2 PLENILUNIO</nombre>" +
 				"<url>http://lalocurademao.blogspot.com.es/2013/04/review-o2-plenilunio.html</url>" +
-				"<nota>-</nota>" +
-				"<mobiliario>-</mobiliario>" +
-				"<personal>-</personal>" +
-				"<precio>-</precio>" +
-				"<masificacion>-</masificacion>" +
-				"<taquillas>-</taquillas>" +
+				"<nota>7.8</nota>" +
+				"<mobiliario>8.8</mobiliario>" +
+				"<personal>8.9</personal>" +
+				"<precio>4</precio>" +
+				"<masificacion>8</masificacion>" +
+		  	"<localizacion>8</localizacion>" +
+			  "<mantenimiento></mantenimiento>" +
+			  "<sedes>14</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>859</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>50</pesoMaxMancuerna>" +
 			"</gimnasio>" +
 		
 			"<gimnasio>" +
 				"<nombre>PALESTRA RIVAS</nombre>" +
 				"<url>http://lalocurademao.blogspot.com.es/2013/04/review-palestra-gym-rivas.html</url>" +
-				"<nota>-</nota>" +
-				"<mobiliario>-</mobiliario>" +
-				"<personal>-</personal>" +
-				"<precio>-</precio>" +
-				"<masificacion>-</masificacion>" +
-				"<taquillas>-</taquillas>" +
+				"<nota>8</nota>" +
+				"<mobiliario>9</mobiliario>" +
+				"<personal>7.5</personal>" +
+				"<precio>4</precio>" +
+				"<masificacion>7</masificacion>" +
+		  	"<localizacion>8</localizacion>" +
+			  "<mantenimiento>8</mantenimiento>" +
+			  "<sedes>2</sedes>" +
+			 "<extras>-</extras>" +
+				"<taquillas>859</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>50</pesoMaxMancuerna>" +
 			"</gimnasio>" +
 	
 			"<gimnasio>" +
-				"<nombre>POLIDEPORTIVOS AND FITNESS</nombre>" +
+				"<nombre>POLIDEPORTIVOS AND FITNESS TELEFONICA</nombre>" +
 				"<url>http://lalocurademao.blogspot.com.es/2011/-/cuidado-con-los-gimnasios.html</url>" +
-				"<nota>-</nota>" +
-				"<mobiliario>-</mobiliario>" +
-				"<personal>-</personal>" +
-				"<precio>-</precio>" +
-				"<masificacion>-</masificacion>" +
+				"<nota>3.5</nota>" +
+				"<mobiliario>6.5</mobiliario>" +
+				"<personal>4.5</personal>" +
+				"<precio>4</precio>" +
+				"<masificacion>4</masificacion>" +
+		  	"<localizacion>8</localizacion>" +
+			  "<mantenimiento>4</mantenimiento>" +
+			  "<sedes>10</sedes>" +
+			 "<extras>-</extras>" +
 				"<taquillas>-</taquillas>" +
+				"<duchas>15</duchas>" +
+				"<pesoMaxMancuerna>-</pesoMaxMancuerna>" +
 			"</gimnasio>" +
 			
 		"</gimnasios>";
