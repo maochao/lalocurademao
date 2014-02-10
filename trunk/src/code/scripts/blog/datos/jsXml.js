c46b264ebfec3560f14e08ad79fa0c0ad45d0723
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2014-02-10 11:56:09 +0000

    nuevos datos para ranking gyms

--- trunk/src/code/scripts/blog/datos/jsXml.js
+++ trunk/src/code/scripts/blog/datos/jsXml.js
@@ -33,11 +33,35 @@ $xmlDoc.find("gimnasio").each(function (){
 		var masificacion = $(this).find("masificacion").text();
 		var textoMasificacion = '<td align="center"><span>'+masificacion+'</span></td>';
 		texto = texto + textoMasificacion;
+
+		var localizacion = $(this).find("localizacion").text();
+		var textoLocalizacion = '<td align="center"><span>'+localizacion+'</span></td>';
+		texto = texto + textoLocalizacion;
+
+		var mantenimiento = $(this).find("mantenimiento").text();
+		var textoMantenimiento = '<td align="center"><span>'+mantenimiento+'</span></td>';
+		texto = texto + textoMantenimiento;
+
+		var sedes = $(this).find("sedes").text();
+		var textoSedes = '<td align="center"><span>'+sedes+'</span></td>';
+		texto = texto + textoSedes;
+
+		var extras = $(this).find("extras").text();
+		var textoExtras = '<td align="center"><span>'+extras+'</span></td>';
+		texto = texto + textoExtras;
 		
 		var taquillas = $(this).find("taquillas").text();
 		var textoTaquillas = '<td align="center"><span>'+taquillas+'</span></td>';
 		texto = texto + textoTaquillas;
-		
+
+		var duchas = $(this).find("duchas").text();
+		var textoDuchas = '<td align="center"><span>'+duchas+'</span></td>';
+		texto = texto + textoDuchas;
+
+		var pesoMaxMancuerna = $(this).find("pesoMaxMancuerna").text();
+		var textoPesoMaxMancuerna = '<td align="center"><span>'+pesoMaxMancuerna+'</span></td>';
+		texto = texto + textoPesoMaxMancuerna;
+
 		texto = texto + '</tr>';
 		
 		$('#tabla tbody').append(texto);
