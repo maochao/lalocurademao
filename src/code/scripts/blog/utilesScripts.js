158d057a693d6af973fd5625185f857a829065b1
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2014-02-21 14:18:11 +0000

    FLECOS

--- src/code/scripts/blog/utilesScripts.js
+++ src/code/scripts/blog/utilesScripts.js
@@ -31,7 +31,8 @@
 		    }
 			  if (typeof fileref != "undefined") {
 					document.getElementsByTagName("head")[0].appendChild(fileref);
-			}
+        }
+    }
 		
 		function seleccionarEstilosTipoBlog(){
 	
