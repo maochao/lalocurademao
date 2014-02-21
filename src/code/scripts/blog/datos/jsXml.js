7fe50af7ad1d72c3a0d3365d9da304647e08ce16
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2014-02-21 14:22:39 +0000

    FLECOS

--- src/code/scripts/blog/datos/jsXml.js
+++ src/code/scripts/blog/datos/jsXml.js
@@ -1,103 +1,69 @@
-$(document).ready(function () {
-	cargarDatos("NOFILTRAR");
-	rellenarComboCiudades();
-	filtrarPorCiudad();
-	});
+$(document).ready(function () {cargarDatos()});
 
-function formarTexto(texto){
-	
-		var nombre = $(this).find("nombre").text();
-		var url = $(this).find("url").text();
-		var textoNombre = '<td><a href="'+url+'"><span>'+nombre+'</span></a></td>';
-		texto = texto + textoNombre;
-		
-		var nota = $(this).find("nota").text();
-		var textoNota = '<td align="center"><span>'+nota+'</span></td>';
-		texto = texto + textoNota;
-		
-		var mobiliario = $(this).find("mobiliario").text();
-		var textoMobiliario = '<td align="center"><span>'+mobiliario+'</span></td>';
-		texto = texto + textoMobiliario;
-		
-		var personal = $(this).find("personal").text();
-		var textoPersonal = '<td align="center"><span>'+personal+'</span></td>';
-		texto = texto + textoPersonal;
-		
-		var precio = $(this).find("precio").text();
-		var textoPrecio = '<td align="center"><span>'+precio+'</span></td>';
-		texto = texto + textoPrecio;
-		
-		var masificacion = $(this).find("masificacion").text();
-		var textoMasificacion = '<td align="center"><span>'+masificacion+'</span></td>';
-		texto = texto + textoMasificacion;
+function cargarDatos () {
 
-		var localizacion = $(this).find("localizacion").text();
-		var textoLocalizacion = '<td align="center"><span>'+localizacion+'</span></td>';
-		texto = texto + textoLocalizacion;
+_xml =  $.parseXML(archivoGimnasios);
+$xmlDoc = $(_xml);
+$('#myTable tbody tr').remove();
+$xmlDoc.find("gimnasio").each(function (){
+                
+                var texto = '<tr>';
+                
+                var nombre = $(this).find("nombre").text();
+                var url = $(this).find("url").text();
+                var textoNombre = '<td><a id="nombre" href="'+url+'"><span>'+nombre+'</span></a></td>';
+                texto = texto + textoNombre;
+                
+                var nota = $(this).find("nota").text();
+                var textoNota = '<td align="center"><span>'+nota+'</span></td>';
+                texto = texto + textoNota;
+                
+                var mobiliario = $(this).find("mobiliario").text();
+                var textoMobiliario = '<td align="center"><span>'+mobiliario+'</span></td>';
+                texto = texto + textoMobiliario;
+                
+                var personal = $(this).find("personal").text();
+                var textoPersonal = '<td align="center"><span>'+personal+'</span></td>';
+                texto = texto + textoPersonal;
+                
+                var precio = $(this).find("precio").text();
+                var textoPrecio = '<td align="center"><span>'+precio+'</span></td>';
+                texto = texto + textoPrecio;
+                
+                var masificacion = $(this).find("masificacion").text();
+                var textoMasificacion = '<td align="center"><span>'+masificacion+'</span></td>';
+                texto = texto + textoMasificacion;
 
-		var mantenimiento = $(this).find("mantenimiento").text();
-		var textoMantenimiento = '<td align="center"><span>'+mantenimiento+'</span></td>';
-		texto = texto + textoMantenimiento;
+                var localizacion = $(this).find("localizacion").text();
+                var textoLocalizacion = '<td align="center"><span>'+localizacion+'</span></td>';
+                texto = texto + textoLocalizacion;
 
-		var sedes = $(this).find("sedes").text();
-		var textoSedes = '<td align="center"><span>'+sedes+'</span></td>';
-		texto = texto + textoSedes;
+                var mantenimiento = $(this).find("mantenimiento").text();
+                var textoMantenimiento = '<td align="center"><span>'+mantenimiento+'</span></td>';
+                texto = texto + textoMantenimiento;
 
-		var extras = $(this).find("extras").text();
-		var textoExtras = '<td align="center"><span>'+extras+'</span></td>';
-		texto = texto + textoExtras;
-		
-		var taquillas = $(this).find("taquillas").text();
-		var textoTaquillas = '<td align="center"><span>'+taquillas+'</span></td>';
-		texto = texto + textoTaquillas;
+                var sedes = $(this).find("sedes").text();
+                var textoSedes = '<td align="center"><span>'+sedes+'</span></td>';
+                texto = texto + textoSedes;
 
-		var duchas = $(this).find("duchas").text();
-		var textoDuchas = '<td align="center"><span>'+duchas+'</span></td>';
-		texto = texto + textoDuchas;
+                var extras = $(this).find("extras").text();
+                var textoExtras = '<td align="center"><span>'+extras+'</span></td>';
+                texto = texto + textoExtras;
+                
+                var taquillas = $(this).find("taquillas").text();
+                var textoTaquillas = '<td align="center"><span>'+taquillas+'</span></td>';
+                texto = texto + textoTaquillas;
 
-		var pesoMaxMancuerna = $(this).find("pesoMaxMancuerna").text();
-		var textoPesoMaxMancuerna = '<td align="center"><span>'+pesoMaxMancuerna+'</span></td>';
-		texto = texto + textoPesoMaxMancuerna;
+                var duchas = $(this).find("duchas").text();
+                var textoDuchas = '<td align="center"><span>'+duchas+'</span></td>';
+                texto = texto + textoDuchas;
 
-	return texto;
-}
+                var pesoMaxMancuerna = $(this).find("pesoMaxMancuerna").text();
+                var textoPesoMaxMancuerna = '<td align="center"><span>'+pesoMaxMancuerna+'</span></td>';
+                texto = texto + textoPesoMaxMancuerna;
 
-function cargarDatos (filtrar) {
-	
-	_xml =  $.parseXML(archivoGimnasios);
-	$xmlDoc = $(_xml);
-	$('#myTable tbody tr').remove();
-	$xmlDoc.find("gimnasio").each(function (){
-		
-		var texto = '<tr>';
-
-		texto = formarTexto(texto);
-
-	texto = texto + '</tr>';
-
-		var ciudad = $(this).find("ciudad").text();
-	if (!existeEnArray(ciudad,arrayCiudades)){
-		arrayCiudades.push(ciudad);
-	}
-		
-		$('#myTable tbody').append(texto);
+                texto = texto + '</tr>';
+                
+                $('#myTable tbody').append(texto);
 })
 };
-
-function rellenarComboCiudades(){
-    $.each(arrayCiudades, function(val, text) {
-        $('#ciudades').append(
-            $('<option></option>').val(val).html(text)
-        );            
-})}
-
-function filtrarPorCiudad(){
-	 $("#ciudades").change(function(){
-         var op = $("#ciudades option:selected");
-         ciudadSeleccionada(op.text());
- });
-}
-
-function ciudadSeleccionada(ciudad){
-	cargarDatos(ciudad);
-}
