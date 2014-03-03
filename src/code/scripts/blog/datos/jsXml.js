d4474edd6c8b4cc820b0988fa1ce98d0a0709cc1
maochao89@gmail.com <maochao89@gmail.com@340ac609-711b-835c-51c5-9b311aa96c5f>
2014-03-03 17:34:12 +0000

    VERSIÓN FINAL PARA TIPOS GYMS

--- src/code/scripts/blog/datos/jsXml.js
+++ src/code/scripts/blog/datos/jsXml.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 	rellenarComboCiudades();
 	rellenarComboTipos();
 	filtrarPorCiudad();
+	filtrarPorTipo();
 	});
 
 function rellenarComboCiudades(){
@@ -26,6 +27,13 @@ function filtrarPorCiudad(){
  });
 }
 
+function filtrarPorTipo(){
+	 $("#tipos").change(function(){
+        var op = $("#tipos option:selected");
+        cargarDatosTipos(op.text());
+});
+}
+
 function establecerTipoGym(tipo){
     var tipoGym = "";
     
@@ -42,6 +50,90 @@ function establecerTipoGym(tipo){
     
     return tipoGym;
 }
+
+function cargarDatosTipos(tipo) {
+
+	var tipoCombo = tipo;
+	_xml =  $.parseXML(archivoGimnasios);
+	$xmlDoc = $(_xml);
+	$('#myTable tbody tr').remove();
+	$xmlDoc.find("gimnasio").each(function (){
+            
+    var texto = '<tr>';
+    
+    var tipoGym = $(this).find("tipo").text();
+	if (!existeEnArray(tipoGym,arrayTipos)){
+		arrayTipos.push(establecerTipoGym(tipoGym));
+	}
+	
+	  if(tipoCombo == establecerTipoGym(tipoGym) || tipoCombo == "SELECCIONA UN TIPO"){  
+		  
+	        var foto = $(this).find("foto").text();
+	        var textoFoto = '<td><img width="50" heigth="50" src="'+foto+'"/></td>';
+	        texto = texto + textoFoto;
+		  
+		    var nombre = $(this).find("nombre").text();
+		    var url = $(this).find("url").text();
+		    var textoNombre = '<td><a id="nombre" width="600" href="'+url+'"><span>'+nombre+'</span></a></td>';
+		    texto = texto + textoNombre;
+		    
+		    var nota = $(this).find("nota").text();
+		    var textoNota = '<td align="center" width="5" width="5"><span>'+nota+'</span></td>';
+		    texto = texto + textoNota;
+		    
+		    var mobiliario = $(this).find("mobiliario").text();
+		    var textoMobiliario = '<td align="center" width="5"><span>'+mobiliario+'</span></td>';
+		    texto = texto + textoMobiliario;
+		    
+		    var personal = $(this).find("personal").text();
+		    var textoPersonal = '<td align="center" width="5"><span>'+personal+'</span></td>';
+		    texto = texto + textoPersonal;
+		    
+		    var precio = $(this).find("precio").text();
+		    var textoPrecio = '<td align="center" width="5"><span>'+precio+'</span></td>';
+		    texto = texto + textoPrecio;
+		    
+		    var masificacion = $(this).find("masificacion").text();
+		    var textoMasificacion = '<td align="center" width="5"><span>'+masificacion+'</span></td>';
+		    texto = texto + textoMasificacion;
+		
+		    var localizacion = $(this).find("localizacion").text();
+		    var textoLocalizacion = '<td align="center" width="5"><span>'+localizacion+'</span></td>';
+		    texto = texto + textoLocalizacion;
+		
+		    var mantenimiento = $(this).find("mantenimiento").text();
+		    var textoMantenimiento = '<td align="center" width="5"><span>'+mantenimiento+'</span></td>';
+		    texto = texto + textoMantenimiento;
+		
+		    var sedes = $(this).find("sedes").text();
+		    var textoSedes = '<td align="center" width="5"><span>'+sedes+'</span></td>';
+		    texto = texto + textoSedes;
+		
+		    var extras = $(this).find("extras").text();
+		    var textoExtras = '<td align="center" width="5"><span>'+extras+'</span></td>';
+		    texto = texto + textoExtras;
+		    
+		    var taquillas = $(this).find("taquillas").text();
+		    var textoTaquillas = '<td align="center" width="5"><span>'+taquillas+'</span></td>';
+		    texto = texto + textoTaquillas;
+		
+		    var duchas = $(this).find("duchas").text();
+		    var textoDuchas = '<td align="center" width="5"><span>'+duchas+'</span></td>';
+		    texto = texto + textoDuchas;
+		
+		    var pesoMaxMancuerna = $(this).find("pesoMaxMancuerna").text();
+		    var textoPesoMaxMancuerna = '<td align="center" width="5"><span>'+pesoMaxMancuerna+'</span></td>';
+		    texto = texto + textoPesoMaxMancuerna;
+		    
+		    var tipo = $(this).find("tipo").text();
+		    var textoTipo = '<td align="center" width="5"><span>'+establecerTipoGym(tipo)+'</span></td>';
+		    texto = texto + textoTipo;
+	  }
+	    
+    texto = texto + '</tr>';
+    
+    $('#myTable tbody').append(texto);})
+};
 
 function cargarDatosCiudad(ciudad) {
 
